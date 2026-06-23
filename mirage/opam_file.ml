ac82cebfb5fe817d50475e2375f7858674c0cc5a
Hannes Mehnert <hannes@mehnert.org>
2026-06-23 11:32:29 +0200

    Deal with OpamParser.FullPos.string raising an exception
    as discussed on #58

--- mirage/opam_file.ml
+++ mirage/opam_file.ml
@@ -128,20 +128,23 @@ let extract_urls filename str =
      extra-source NAME { src: URL checksum: [ STRING ] } (OR checksum: STRING) <- multiple occurences possible
   *)
   let open OpamParserTypes.FullPos in
-  let opamfile = OpamParser.FullPos.string str filename in
-  let unavailable =
-    List.exists
-      (function
-        | { pelem = Variable ({ pelem = "available" ; _ },
-                              { pelem = (Bool false | List { pelem = [{ pelem = Bool false; _ }] ; _ }); _ })
-          ; _ } -> true
-        | _ -> false)
-      opamfile.file_contents
-  in
-  if unavailable then
-     [], []
-  else
-    extract_checksums_and_urls filename opamfile
+  match OpamParser.FullPos.string str filename with
+  | exception e ->
+    [], [ `Msg ("error reading filename " ^ Printexc.to_string e) ]
+  | opamfile ->
+    let unavailable =
+      List.exists
+        (function
+          | { pelem = Variable ({ pelem = "available" ; _ },
+                                { pelem = (Bool false | List { pelem = [{ pelem = Bool false; _ }] ; _ }); _ })
+            ; _ } -> true
+          | _ -> false)
+        opamfile.file_contents
+    in
+    if unavailable then
+      [], []
+    else
+      extract_checksums_and_urls filename opamfile
 
 type repo = {
   upstream : string;
